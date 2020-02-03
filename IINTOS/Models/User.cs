91bde12b54769015f032127e47bab855b2caba45
Patrick Battisti Forsthofer <190200007@estudantes.ips.pt>
2020-02-03 11:38:29 +0000

    database update

--- IINTOS/Models/User.cs
+++ IINTOS/Models/User.cs
@@ -16,12 +16,13 @@ namespace IINTOS.Models
         /// Name of the user
         /// </summary>
         public String Name { get; set; }
-        
+
         /// <summary>
         /// A small introduction to the user
         /// </summary>
+        #nullable enable
         public String? About { get; set; }
-        
+
         /// <summary>
         /// Navigation Property for the nationality
         /// </summary>
